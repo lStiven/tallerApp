02d15849cc1efc9e24f30977c3beb864b364979f
Ian2012 <crisgarta8@gmail.com>
2021-02-18 18:35:20 -0500

    Acciones del menú separadas para futura implementación

--- app/src/main/java/com/example/taller2/menus/GlobalMenu.java
+++ app/src/main/java/com/example/taller2/menus/GlobalMenu.java
@@ -2,7 +2,10 @@ package com.example.taller2.menus;
 
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.taller2.R;
@@ -13,5 +16,23 @@ public class GlobalMenu extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.global_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "item1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "item2", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "item3", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
