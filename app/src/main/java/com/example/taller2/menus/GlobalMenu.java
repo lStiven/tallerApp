1bd56f78a58453d0af5705dce70655b205626a0a
Ian2012 <crisgarta8@gmail.com>
2021-02-18 21:50:31 -0500

    Detección correcta de la conexión a internet en todas las actividades.

--- app/src/main/java/com/example/taller2/menus/GlobalMenu.java
+++ app/src/main/java/com/example/taller2/menus/GlobalMenu.java
@@ -1,7 +1,12 @@
 package com.example.taller2.menus;
 
-import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,8 +19,27 @@ import com.example.taller2.R;
 import com.example.taller2.activity.AuxiliarActivity;
 import com.example.taller2.activity.LoginActivity;
 import com.example.taller2.activity.MainActivity;
+import com.example.taller2.databinding.ActivityLoginBinding;
+import com.example.taller2.services.CheckConectivity;
 
 public class GlobalMenu extends AppCompatActivity {
+    protected ActivityLoginBinding binding;
+
+    public static final String BroadcastStringForAction = "checkinternet";
+
+    protected IntentFilter mIntentFilter;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startService(new Intent(this, CheckConectivity.class));
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(BroadcastStringForAction);
+
+        Intent i = new Intent(getApplicationContext(), CheckConectivity.class);
+        startService(i);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,21 +53,21 @@ public class GlobalMenu extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.item1:
                 Toast.makeText(this, "item1", Toast.LENGTH_SHORT).show();
-                Intent i=new  Intent(this, LoginActivity.class);
+                Intent i = new Intent(this, LoginActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
 
                 return true;
             case R.id.item2:
                 Toast.makeText(this, "item2", Toast.LENGTH_SHORT).show();
-                i=new  Intent(this, MainActivity.class);
+                i = new Intent(this, MainActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
 
                 return true;
             case R.id.item3:
                 Toast.makeText(this, "item3", Toast.LENGTH_SHORT).show();
-                i=new  Intent(this, AuxiliarActivity.class);
+                i = new Intent(this, AuxiliarActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
                 return true;
@@ -51,5 +75,40 @@ public class GlobalMenu extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    private BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(BroadcastStringForAction)) {
+                if (!intent.getStringExtra("online_status").equals("true")) {
+
+                    ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+                    ComponentName cn = am.getRunningTasks(1).get(0).topActivity;
+
+                    if (!("class " + cn.getClassName()).equals(LoginActivity.class.toString())) {
+                        startActivity(new Intent(context, LoginActivity.class));
+                    }
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        registerReceiver(myReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(myReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(myReceiver, mIntentFilter);
     }
 }
